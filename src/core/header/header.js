31df6bc61a8bf0116a4ac86cd490eb60d9ec82f8
Abhilekh Dowerah <abhilekh.dowerah@borgos.in>
2021-12-29 15:55:13 +0530

    Header created

--- src/core/header/header.js
+++ src/core/header/header.js
@@ -5,6 +5,7 @@ class Header extends Component{
     render() {
         return(
             <div className={styles.container}>
+                Task Manager Application
             </div>
         )
     }
