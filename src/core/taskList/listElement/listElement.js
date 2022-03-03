d1d08c30b97b6be786f4432f469863c6158f0f69
abhilekhji <abhilekhdowerah@gmail.com>
2022-03-03 11:13:35 +0530

    implemented edit and delete button

--- src/core/taskList/listElement/listElement.js
+++ src/core/taskList/listElement/listElement.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import styles from './styles.module.css';
 class ListElement extends Component {
     render() {
@@ -6,6 +7,10 @@ class ListElement extends Component {
         return(
             <div className={styles.container}>
                 <div className={isCompleted==='true'?styles.completedTask:styles.incompletedTask}>{name}</div>
+                <div>
+                    <AiFillEdit className={styles.icon}/>
+                    <AiFillDelete className={styles.icon}/>
+                </div>
             </div>
         );
     }
--- src/core/taskList/taskList.js
+++ src/core/taskList/taskList.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
 import ListElement from './listElement';
 import styles from './styles.module.css';
-import data from './data';
+import axios from 'axios';
+// import data from './data';
 
 class TaskList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
+
+    componentDidMount = () => {
+        this.fetchAllTasks();
+    }
+
+    fetchAllTasks = async () => {
+        const config = {
+            method: 'get',
+            url: 'http://localhost:8000/api/tasks/getAllTasks',
+        };
+          
+        axios(config)
+        .then(function (response) {
+            console.log("ahise: ",response.data);
+        })
+        .catch(function (error) {
+            console.log("nai aha: ", error);
+        });
+    }
+
     render() {
-        console.log(data);
+        const { data } = this.state;
         return (
             <div className={styles.container}>
                 {data.map((item)=><ListElement key={item._id} name={item.name} isCompleted={item.completed}/>)}
